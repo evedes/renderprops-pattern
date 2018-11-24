c2285523ecd96e9a31f0db810497f5978d3d322a
evedes <eduardo.vedes@gmail.com>
2018-11-24 19:03:12 +0000

    exercise commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
-class App extends Component {
+// const Temperature = ({ children }) => children(20);
+
+class Temperature extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+      { this.props.children(20) }
+      </Fragment>
     );
   }
 }
 
-export default App;
+const saySomethingAboutTheWeather = (temp) => {
+  return temp >= 30 ? 'My brain is melting!' : 'My brain is not melting';
+}
+
+const ShowTemperatureInCelsius = ({temp, color}) => {
+  return (
+    <div>
+      <h2 style={{ color: `${color}` }}>Temp is {temp} ºC</h2>
+      { saySomethingAboutTheWeather(temp) }
+    </div>
+  );
+}
+
+const ShowTemperatureInFahrenheit = ({temp}) => {
+  const tempInF = temp * (9/5) +32;
+  return (
+    <div>
+      <h2>Temp is {tempInF} ºF</h2>
+      { saySomethingAboutTheWeather(tempInF) }
+    </div>
+  )
+}
+
+const App = ({color}) => {
+    return (
+      <div className="text-center my-3">
+        <Temperature>
+          { (temp) => (
+            <Fragment>
+              <ShowTemperatureInCelsius temp={temp} color={color} />
+              <ShowTemperatureInFahrenheit temp={temp} />
+            </Fragment>
+            )
+          }
+        </Temperature>
+      </div>
+    );
+}
+
+const withColor = WrappedComponent => {
+  return class extends React.Component {
+    render() {
+      return <WrappedComponent color={'red'} />;
+    }
+  }
+}
+
+
+export default withColor(App);
